61765dcbbc69a2fd2de484a67c1837915da88e45
takanori_miyadera <takawork16@gmail.com>
2021-02-24 21:50:16 +0900

    日本語のバリデートエラー時のメッセージ追加 エラー時の処理追加 APIに投げるときに、特定のパラメーターのみ使用するように修正 APIに投げるときに、タイムアウト、リトライ回数追加 バリデート追加

--- config/config.php
+++ config/config.php
@@ -1,4 +1,11 @@
 <?php
 return [
   'RECRUIT_API_KEY' => env('RECRUIT_API_KEY'),
+  'HOTPEPPER_API' => [
+    'TIMEOUT' => 5,
+    'RETRY' => [
+      'TIMES' => 3,
+      'WAIT' => 100
+    ]
+  ]
 ];
